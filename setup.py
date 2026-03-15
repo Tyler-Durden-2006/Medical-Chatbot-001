2408a44a0f01557f807798434e92920bc8098882
Sarthak Singh Rawat <sarthak@Sarthaks-MacBook-Air.local>
2026-03-15 13:43:32 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='medical-chatbot',
+    version="0.1.0",
+    author="Sarthak Singh Rawat",
+    author_email="sarthakrawat1092@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
